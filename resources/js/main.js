a38437d4fba1eee1f5c44f4799e8f2f42d003164
A S M TARIK KONOCK <you@example.com>
2020-03-26 23:35:09 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+ //STICKY MENU
+  $(".js--services-section").waypoint(function(direction){
+     if(direction=="down"){
+        $("nav").addClass("sticky");
+        }else{
+         $("nav").removeClass("sticky");
+        }
+  });
+
+
+    //MIXITUP(PORTFOLIO SECTION)
+ var mixer = mixitup('.container');
+
+         
+  //SMOOTH SCROLL FOR IE/EDGE/SAFARI
+
+$("a").on('click', function(event){
+   if(this.hash!==""){
+       event.preventDefault();
+       var hash=this.hash;
+
+       $('html,body').animate({
+           scrollTop:$(hash).offset().top
+           },800,function(){
+           window.location.hash=hash;
+       });
+
+      }
+});
+
+});
+
+//MOBILE MENU
+function openNav(){
+ document.getElementById("myNav").style.width= "100%";
+}
+function closeNav(){
+ document.getElementById("myNav").style.width= "0%";
+}
+
+
+// ACTIVE LINK
+    $("nav ul li a").click(function(){
+        $("nav ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("nav .logo").click(function(){
+        $("nav ul li a").removeClass("active");
+        $("nav ul li:first-child a").addClass("active");
+    });
+
+
+
+
+
+
+
+
